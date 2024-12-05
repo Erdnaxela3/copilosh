b1a6d3e23410e0bea2f33e8d045a85ce398946d1
Alexandre WENG <alexandre.weng@epita.fr>
2024-12-05 21:35:42 +0100

    test: added logging to create_response_dataset.py

--- tests/create_response_dataset.py
+++ tests/create_response_dataset.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from dataclasses import asdict, dataclass
 
@@ -31,6 +32,9 @@ class ResponseFromModel:
     time: float
 
 
+logger = logging.getLogger(__name__)
+
+
 def user_prompt_formatter(cmd_error: CmdError) -> str:
     """
     Format the user prompt from the command error, that will be used to generate a response.
@@ -47,18 +51,30 @@ Help me fix it.
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("copilosh").setLevel(logging.INFO)
+
     with open("test_suite_results.yml", "r") as f:
         cmd_errors = [CmdError(**err) for err in yaml.safe_load(f)]
 
     with open("system_prompts.yml", "r") as f:
         system_prompts = [SystemPrompt(**sp) for sp in yaml.safe_load(f)]
 
+    n_sp, n_errors, n_models = len(system_prompts), len(cmd_errors), len(model_to_class)
+    n_responses = n_sp * n_errors * n_models
+    logger.info(f"Creating dataset with {n_responses} generated responses")
+
     responses = []
-    for model_name, model_cls in model_to_class.items():
+    response_i = 0
+    for model_no, (model_name, model_cls) in enumerate(model_to_class.items()):
         model = model_cls()
         model.load_model()
-        for cmd_err in cmd_errors:
-            for sp in system_prompts:
+        for err_no, cmd_err in enumerate(cmd_errors):
+            for sp_no, sp in enumerate(system_prompts):
+                response_i += 1
+                logger.info(
+                    f"Response {response_i}/{n_responses} - Model {model_no + 1}/{n_models} - Error {err_no + 1}/{n_errors} - System Prompt {sp_no + 1}/{n_sp} ({model_name}, {cmd_err.id}, {sp.id})"
+                )
                 user_prompt = user_prompt_formatter(cmd_err)
                 start_time = time.time()
                 response = model.get_response(system_prompt=str(sp), user_prompt=user_prompt)
