3037315a3e5e264aa424f6fb9f42bdca9bd21600
Alexandre WENG <alexandre.weng@epita.fr>
2024-12-05 21:24:15 +0100

    test: moved tests in subfolder
    - for easier navigation in the tests directory
    - added cwd argument to subprocess run to take the directory change into
      account

--- tests/create_test_suite.py
+++ tests/create_test_suite.py
@@ -15,23 +15,30 @@ class CmdError:
     stdout: str
 
 
-def run_shell_scripts_and_collect_data(directory):
+def run_shell_scripts_and_collect_data(directory: str):
     """
     Execute all `.sh` files in the specified directory and collect error information.
+
+    :param directory: The directory containing the `.sh` files
     """
     results = []
     id = 0
     for filename in os.listdir(directory):
         if filename.endswith(".sh"):
-            script_path = os.path.join(directory, filename)
-            command = f"bash {script_path}"
+            script_path_from_cwd = os.path.join(directory, filename)
+            script_path_from_directory = os.path.abspath(script_path_from_cwd)
+            command = f"bash {script_path_from_directory}"
 
-            with open(script_path, "r") as script_file:
+            with open(script_path_from_cwd, "r") as script_file:
                 script_content = script_file.read()
             try:
                 # Run the shell script and capture output and error
                 process = subprocess.run(
-                    command, shell=True, capture_output=True, text=True
+                    command,
+                    shell=True,
+                    capture_output=True,
+                    text=True,
+                    cwd=directory,
                 )
                 results.append(
                     CmdError(
@@ -47,7 +54,7 @@ def run_shell_scripts_and_collect_data(directory):
                 results.append(
                     CmdError(
                         id=id,
-                        command=command,
+                        command=script_content,
                         return_code=-1,
                         script=filename,
                         stderr=f"Failed to execute script: {str(e)}",
@@ -69,7 +76,7 @@ def save_results_to_yaml(results, output_file):
 
 def main():
     # Specify the directory containing the `.sh` files
-    scripts_directory = "./"  # Change this if `.sh` files are in another directory
+    scripts_directory = "./test_scripts"  # Change this if `.sh` files are in another directory
     output_yaml = "test_suite_results.yml"
 
     # Run the shell scripts and collect data
